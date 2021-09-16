2a8e375974eee684c06184c375f58e6cbad23384
Sebastián Pérez Etchandy <sebastian@peretch.com>
2021-09-16 01:10:48 -0300

    Added more variable type annotation examples

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -1,17 +1,18 @@
-// ------------- //
+// -------------------------- //
 // Variables type annotations
-// ------------- //
+// -------------------------- //
+// IF POSSIBLE, ALLWAYS USE TYPE INFERENFCE
 
 // This is a variable type annotation
-const apples: number = 5;
-const speed: string = 'fast';
-const hasName: boolean = true;
+const apples: number = 5; // Not necesary, can be inferenced
+const speed: string = 'fast'; // Not necesary, can be inferenced
+const hasName: boolean = true; // Not necesary, can be inferenced
 
-const nothingMuch: null = null;
-const nothing: undefined = undefined;
+const nothingMuch: null = null; // Not necesary, can be inferenced
+const nothing: undefined = undefined; // Not necesary, can be inferenced
 
 // built in objects
-const now: Date = new Date();
+const now: Date = new Date(); // Not necesary, can be inferenced
 
 // Array
 const colors: string[] = ['yellow', 'black', 'green'];
@@ -35,3 +36,38 @@ const otherPoint: { x: number; y: number } = {
   x: 10,
   y: 20,
 };
+
+// Function
+// We tell typescript inside and output types
+// it spect to receive (i: number)
+// it spect to return nothing (void)
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// WHEN TO USE ANNOTATIONS
+// 1) Fucntion that returns the 'any' type
+const json = '{"X": 10, "y": 20}';
+// const coordinates = JSON.parse(json); // infered annotation is 'any'
+const coordinates: { x: number; y: number } = JSON.parse(json);
+console.log(coordinates); // {x: 10, y:20}
+
+// 2) Declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+// let foundWorld; // infered annotation is 'any'
+let foundWorld: boolean; // infered annotation is 'any'
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWorld = true;
+  }
+}
+
+// 3) Variable whose type that can't be inferred
+let numbersList = [-10, -1, 12];
+// let numberAboveZero = false; // infered annotation is 'boolean'
+let numberAboveZero: boolean | number = false; // May be boolean or number
+for (let i = 0; i < numbersList.length; i++) {
+  if (numbersList[i] > 0) {
+    numberAboveZero = numbersList[i];
+  }
+}
